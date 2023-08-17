ac6a682d9c52cf31494b55c2e09694e182f4d27e
senseiX21 <mahdix2118@protonmail.com>
2023-08-17 14:02:03 +0100

    Added services

--- index.ts
+++ index.ts
@@ -24,19 +24,27 @@ const client = new MongoClient(uri, {
 const connectDB = async (): Promise<void> => {
     try {
         const db: Db = client.db('zmk-electrical');
-        const productCollection: Collection<Document> = db.collection('services')
+        const servicesCollection: Collection<Document> = db.collection('services');
+        const projectCollection: Collection<Document> = db.collection('project');
+
         console.log('Database connection established successfully!')
 
         //Get All Products
         app.get('/services', async (req: Request, res: Response) => {
-            const result = await productCollection.find({}).toArray();
+            const result = await servicesCollection.find({}).toArray();
             res.send(result);
         });
 
         //get single Product
         app.get('/services/:id', async (req: Request, res: Response) => {
             const id = req.params.id;
-            const result = await productCollection.findOne({ _id: new ObjectId(id) });
+            const result = await servicesCollection.findOne({ _id: new ObjectId(id) });
+            res.send(result);
+        });
+
+        //get all projects
+        app.get('/projects', async (req: Request, res: Response) => {
+            const result = await projectCollection.find({}).toArray();
             res.send(result);
         });
 
